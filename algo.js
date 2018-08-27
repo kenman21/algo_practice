109d2f3fff6c360018eb1511d6cc5229a50cc8b5
Kenny Lehr <kenman9009@gmail.com>
2018-08-27 16:04:50 -0400

    completed multiply

--- algo.js
+++ algo.js
@@ -126,6 +126,11 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if (y === 1){
+    return x
+  } else {
+    return x + multiply(x,y-1)
+  }
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
