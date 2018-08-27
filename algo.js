f8afcead22d4c55a5119b1c048f96b8b1529431c
Kenny Lehr <kenman9009@gmail.com>
2018-08-27 15:56:48 -0400

    completed modulo

--- algo.js
+++ algo.js
@@ -116,6 +116,11 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  if (x < y){
+    return x
+  } else {
+    return modulo(x-y,y)
+  }
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
