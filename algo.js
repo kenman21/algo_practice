8abf7b5098d40235f230c57a6ab56c29c6b6e235
Kenny Lehr <kenman9009@gmail.com>
2018-09-10 17:36:21 -0400

    completed countOccurrence

--- algo.js
+++ algo.js
@@ -228,12 +228,25 @@ var fizzBuzz = function(n) {
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
-
+  sum = 0;
+  if (array[0] === value) {
+    sum += 1
+  }
+  if (array.length === 1) {
+    return sum
+  }
+  sum += countOccurrence(array.slice(1), value)
+  return sum
 };
 
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
 var rMap = function(array, callback) {
+  if (array.length === 1){
+
+  } else {
+
+  }
 };
 
 // 22. Write a function that counts the number of times a key occurs in an object.
